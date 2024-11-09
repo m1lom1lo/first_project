0b766ed5f5f44ec8f3531964e962c686a1103a41
m1lom1lo <cameegs.j@gmail.com>
2024-11-09 12:22:50 +0800

    first commit

---
+++ AO.java
@@ -0,0 +1,17 @@
+public class AO{
+	public static void main (String[] args){
+		int A = 5;
+		int B = 10;
+
+		int sum = A+B;
+		int diff = A-B;
+		int prod = A*B;
+		double quo = (double) A/B;
+
+		System.out.println("Sum of 2 numbers is " + sum);
+		System.out.println("Difference of 2 numbers is " + diff);
+		System.out.println("Product of 2 numbers is " + prod);
+		System.out.println("Quotient of 2 numbers is " + quo);
+	}
+}
+
---
+++ sum.java
@@ -0,0 +1,9 @@
+public class sum{
+	public static void main (String[] args){
+		int numa = 10;
+		int numb = 55555555;
+		int sum = numa + numb;
+		System.out.print(sum);
+	}
+}
+	
